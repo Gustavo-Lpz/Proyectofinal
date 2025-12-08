eba506d2c27083778309997e5128d2fbef0bbd45
Gustavo Adolfo López Delgado <hust_mw2000@hotmail.com>
2025-12-07 21:42:56 -0600

    commit 15

--- ecommerce-app/src/app/pages/product-detail/product-detail.component.ts
+++ ecommerce-app/src/app/pages/product-detail/product-detail.component.ts
@@ -3,6 +3,11 @@ import { Product } from '../../core/types/Products';
 import { ProductsService } from '../../core/services/products/products.service';
 import { ActivatedRoute } from '@angular/router';
 import { CurrencyPipe } from '@angular/common';
+import { CartService } from '../../core/services/cart/cart.service';
+import { WishListService } from '../../core/services/wishList/wish-list.service';
+import { Store } from '@ngrx/store';
+import { selectUserId } from '../../core/store/auth/auth.selectors';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-product-detail',
@@ -11,33 +16,68 @@ import { CurrencyPipe } from '@angular/common';
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css'
 })
-export class ProductDetailComponent implements OnInit{
+export class ProductDetailComponent implements OnInit {
   product: Product | null = null;
 
-  constructor(private productService: ProductsService, private route: ActivatedRoute){}
+  constructor(
+    private productService: ProductsService,
+    private route: ActivatedRoute,
+    private cartService: CartService,   // <-- INYECTAR SERVICIO
+    private wishListService: WishListService,
+    private store: Store
+  ) {}
 
   ngOnInit(): void {
     this.route.paramMap.subscribe({
-      next:(params)=>{
-        console.log(params)
+      next: (params) => {
         const id = params.get('id');
-        if (!id) {
-          return
-        }
-        this.productService.getProductByID(id).subscribe({
-          next:(product)=>{
-            this.product = product;
-            console.log(product)
-          },
-          error: (error)=>{
-            this.product = null;
-          }
-        });
-      }
-    })
-    // this.productService.getProductByID();
-  }
-  
+        if (!id) return;
 
+        this.productService.getProductByID(id).subscribe({
+          next: (product) => {
+            this.product = product;
+          },
+          error: () => {
+            this.product = null;
+          },
+        });
+      },
+    });
+  }
+
+  // 🔥 MÉTODO PARA AGREGAR AL CARRITO
+  addToCart(): void {
+    if (!this.product) return;
+
+    this.cartService.addToCart(this.product._id, 1).subscribe();
+  }
+
+  getUserId(): Promise<string> {
+  return new Promise((resolve) => {
+    this.store.select(selectUserId).pipe(take(1)).subscribe(id => {
+      resolve(id ?? '');
+    });
+  });
+}
+
+async addToWishList() {
+  if (!this.product) return;
+
+  const userId = await this.getUserId();
+
+  if (!userId) {
+    console.log('Usuario no autenticado');
+    return;
+  }
+
+  this.wishListService.addProduct(userId, this.product._id).subscribe({
+    next: (wishlist) => {
+      console.log('Agregado a wishlist:', wishlist);
+    },
+    error: (error) => {
+      console.error('Error al agregar a wishlist:', error);
+    }
+  });
+}
 
 }
